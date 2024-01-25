209a9c801607bf21592039775fef0f10b1a61b00
kaymaskaymas <114069552+kaymaskaymas@users.noreply.github.com>
2024-01-25 18:38:33 +0530

    Delete src directory

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    // baseURL: "http://localhost:8080",
-    baseURL: "https://karchisoftservbackend.onrender.com",
-});
-
-export default instance;
